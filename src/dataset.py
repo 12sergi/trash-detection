81c23b0927512dfba2473bb783942d88004c8afa
LuNcer <56787856+12sergi@users.noreply.github.com>
2026-06-13 14:51:05 +0300

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,1 +1,25 @@
+from torchvision import datasets, transforms
 
+def get_datasets():
+    train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])
+
+    valid_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+
+    train_dataset = datasets.ImageFolder(
+        "data/train",
+        transform=train_transform
+    )
+
+    valid_dataset = datasets.ImageFolder(
+        "data/valid",
+        transform=valid_transform
+    )
+
+    return train_dataset, valid_dataset
